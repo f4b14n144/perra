d98df48ce74130c4f68150eadac12aafad263638
Jellz01 <jellzwellesley@gmail.com>
2023-11-13 14:16:00 +0000

    ya camviei

---
+++ prueba/src/main/java/com/mycompany/prueba/Prueba.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prueba;
+
+/**
+ *
+ * @author jellz
+ */
+public class Prueba {
+
+    public static void main(String[] args) {
+        System.out.println("El fabian es mi perra");
+    }
+}
